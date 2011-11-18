e48bb3e601a2dce4c07a2228078d168df4b29bc2
Alex Little <alex@alexlittle.net>
2011-11-18 23:23:14 +0100

    add structure

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,10 @@
 <?php 
 header("Content-type:text/html;charset:utf-8");
 ?>
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-<title>Assessment</title>
+<title>OpenQuiz</title>
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
 
 </head>
--- results.php
+++ results.php
@@ -4,7 +4,11 @@ include_once("config.php");
 include_once("./includes/header.php");
 
 
+include_once("./includes/quizscores.php");
+
 
 
 include_once("./includes/footer.php");
-?>
+
+
+
