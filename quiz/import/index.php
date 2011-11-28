23c31c3da3f4a9747c08663a3361980e631e9691
Alex Little <alex@alexlittle.net>
2011-11-28 19:46:47 +0100

    initial import of gift formatter

--- quiz/import/index.php
+++ quiz/import/index.php
@@ -1,7 +1,25 @@
 <?php 
 
+include_once('format.php');
+include_once('gift_format.php');
+header("Content-type:text/plain;charset:utf-8");
+$file = "testimport.txt";
+if (!file_exists($file)) {
+	echo "file not found";
+	die;
+}
+
+$quiz = file_get_contents($file);
 
 
+print_r($quiz);
 
+print("\n\n----------------------------\n\n");
 
+$import = new qformat_gift();
+print_r($import);
+print("\n\n----------------------------\n\n");
+
+$import->readquestion($quiz);
+//print_r($import);
 ?>
