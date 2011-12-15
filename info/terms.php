262a73c877b89f7eef27508d4db4dfb34663b630
Alex Little <alex@alexlittle.net>
2011-12-15 11:55:15 +0100

    minor text update

--- info/terms.php
+++ info/terms.php
@@ -9,6 +9,21 @@ include_once("../includes/header.php");
 
 <p>The code for mQuiz is available for download (<a href="<?php echo $CONFIG->homeAddress ;?>developer/">visit the developer page for more info</a>), if you would like to run your own mQuiz server, or create your own client application.</p>
 
+
+<h2>License</h2>
+<p>mQuiz is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.</p>
+
+<p>mQuiz is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.</p>
+
+<p>You should have received a copy of the GNU General Public License
+along with mQuiz.  If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>
+
 <?php
 include_once("../includes/footer.php");
 ?>
